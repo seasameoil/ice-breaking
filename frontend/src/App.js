2ff786ebc63248d3c2ed11032570af6af6ed200f
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-08-13 15:28:59 +0900

    feat: 오픈소스 이미지 복사붙혀넣기

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section class="container">
+        <div class="cat">
+          <div class="ears">
+            <p class="ear"></p>
+            <p class="ear"></p>
+          </div>
+          <p class="head">
+            <div class="eyes">
+              <p class="eye"></p>
+              <p class="eye"></p>
+            </div>
+            <p class="nose"></p>
+          </p>
+          <p class="body"></p>
+          <p class="tail"></p>
+          <p class="tail-curl"></p>
+        </div>
+      </section>
     </div>
   );
 }
