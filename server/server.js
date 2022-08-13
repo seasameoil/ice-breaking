c7e7d0e038f3ea790203179f1c41ae49c7a40353
dldks321 <80047618+dldks321@users.noreply.github.com>
2022-08-13 22:17:35 +0900

    [Fix]Fix conflict to main
    Fix conflict to server.js file at main branch

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
-const test = require("./router/test");
+const test = require(".//router/test");
+const question = require("./router/question_list");
+const place = require("./router/place_list");
+//const like = require("./router/like_list");
 
-app.use("/test", test);
+app.use("/", test);
+app.use("/question", question);
+app.use("/place", place);
+//app.use("/", like);
 
 const port = 8080;
 app.listen(port, () => {
